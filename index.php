5c555d14df092d359bff56676a7ddbcab90e4bbb
bhardwaj-ishaan <bhardwaj.ishaan.codes@gmail.com>
2024-07-18 16:23:11 -0700

    fixed the css styles for index

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
-<DOCTYPE html>
+<!DOCTYPE html>
 <html>
     <head>
         <title>Free Games</title>
         <link rel="stylesheet" href="styles.css">   
     </head>
     <body>
-        <header>
+        <div id="header">
         <a href="index.php"><h1>Free Games</h1></a>
             <nav>
                 <a href="signin.php">Sign In</a>
-                <a href="signup.php">Sign Up</a>
+                <a href="create.php">Sign Up</a>
                 <a href="checkout.php">Checkout</a>
             </nav>
-        <header>
+        <div>
     </body>
 </html>
