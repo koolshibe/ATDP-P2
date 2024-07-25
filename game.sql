0ec37ca042b7a7cccb5532436c7736457e896cb4
Ishaan Bhardwaj <bhardwaj.ishaan.codes@gmail.com>
2024-07-24 21:50:43 -0700

    did

--- game.sql
+++ game.sql
@@ -7,7 +7,7 @@ CREATE TABLE games(
 INSERT INTO games (id, game_name, price, img)
 VALUES 
 (1, 'Call uv duty', 999, 'https://cdn2.unrealengine.com/c5s3-download-keyart-1920x1080-f10cd00cc9ad.jpg'),
-(2, 'Gorbinos quest', 1, 'https://static.wikia.nocookie.net/crueltysquad/images/e/e5/Gorbino_texture.png/revision/latest/scale-to-width-down/256?cb=20221211200945');
+(2, 'Gorbinos quest', 1, 'https://cf.geekdo-images.com/WPcGnVhmrbCFfQ36-dvYCA__opengraph/img/VRAxTIkUuHHH5ddSA9AFDKfiyeM=/0x230:989x750/fit-in/1200x630/filters:strip_icc()/pic7884472.png');
 CREATE TABLE purchases(
     id int AUTO_INCREMENT PRIMARY KEY,
     game_id int,
