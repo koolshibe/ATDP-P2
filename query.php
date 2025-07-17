ba374f4ebcc87bfd4c8b117a60d084ca3bb7068f
ikidre <player2@gmail.com>
2025-07-16 22:14:44 -0700

    Revert "Improved sussy query.php"
    This reverts commit 873aedab0bc283f0dc604d5c8a6515b3ca752d38.

--- query.php
+++ query.php
@@ -83,9 +83,6 @@ if (isset($_GET["table"])) {
             $sth -> execute();
         }
     }
-    $sth = $dbh -> prepare("INSERT INTO updates (admin_id, changelog) VALUES (:aid, :msg)"); 
-    $sth -> bindValue(":aid", $_SESSION["aid"]);
-    $sth -> bindValue(":msg", $_POST["msg"]);
 } else {
     header("Location:admindashboard.php");
 }
